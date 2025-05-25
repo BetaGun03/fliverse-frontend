3c51d03c76e96acebb93dfc797dd51c9f3e26f86
BetaGun03 <jaimehedrera25@gmail.com>
2025-05-25 20:42:20 +0200

    Se ha arreglado una parte del componente Content

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
 import { CommonModule, Location } from '@angular/common';
 import { ContentService } from '../../services/content/content.service';
+import { ContentType } from '../../enums/content-type';
 
 @Component({
   selector: 'app-content',
@@ -13,6 +14,7 @@ import { ContentService } from '../../services/content/content.service';
 })
 export class ContentComponent {
 
+  id!: string
   isLoading: boolean = false
   public content!: Content
 
@@ -21,34 +23,54 @@ export class ContentComponent {
 
   constructor(private router: Router, private location: Location, private contentService: ContentService, private route: ActivatedRoute) 
   { 
+    this.id = this.route.snapshot.paramMap.get('id') as string || ''
     const navigation = this.router.getCurrentNavigation()
     this.content = navigation?.extras.state?.['content'] as Content
 
-    this.contentService.getContentAverageRatingById(String(this.content.id))
-    .then(rating => {
-      this.content.average_rating = rating
-    })
-    
-
-    // If content is not provided in the state, fetch it by ID from the route parameters
-    if (!this.content) 
+    if (this.content)
     {
+      // If content is passed through navigation, set it directly
+      this.contentService.getContentAverageRatingById(String(this.content.id))
+        .then(averageRating => {
+          this.content.average_rating = averageRating
+        })
+        .catch(err => {
+          console.error('Error fetching average rating:', err)
+          this.content.average_rating = 0 // Default to 0 if there's an error
+        })
+      this.isLoading = false
+    } 
+    else 
+    {
+      // If no content is passed, fetch it by ID
       this.isLoading = true
-      const id = this.route.snapshot.paramMap.get('id')
-
-      if (id) 
+      if (this.id) 
       {
-        this.contentService.getContentById(id)
+        this.contentService.getContentById(this.id)
           .then(content => {
             this.content = content
             this.isLoading = false
           })
           .catch(err => {
             console.error('Error fetching content by ID:', err)
+            this.content = {
+              id: Number(this.id),
+              title: "Content not found",
+              type: ContentType.MOVIE,
+              synopsis: '',
+              poster: '',
+              release_date: new Date(),
+              average_rating: 0,
+              genre: [],
+              duration: 0,
+              trailer_url: '',
+              keywords: []
+            }
+            this.isLoading = false
           })
       }
-
     }
+
   }
 
   goBack() 
